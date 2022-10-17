4d2f8fedc225cec4cccad26517ef163a0ddd2e34
Nkanga Richard <75099089+Nkanga-Coded-It@users.noreply.github.com>
2022-10-17 12:06:48 +0100

    Initial Commit

---
+++ list on Delete/list_on_DeleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  list_on_DeleteApp.swift
+//  list on Delete
+//
+//  Created by Richard Nkanga on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct list_on_DeleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
